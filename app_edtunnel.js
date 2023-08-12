07c32e472f2855ae530b7ea22186eb45d9b318dd
yoomoo001 <141425926+yoomoo001@users.noreply.github.com>
2023-08-12 17:02:10 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '8b41a80e-0c13-4879-b917-9ce5f79ec7a0';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
